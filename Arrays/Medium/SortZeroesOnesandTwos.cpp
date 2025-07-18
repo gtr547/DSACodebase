8cea792fd1beac1b0018b26f6e5037739da5ef52
gtr547 <gitarthapathak360@gmail.com>
2025-07-19 00:33:15 +0530

    Feat: Sort 0s, 1s and 2s problem solved using Dutch NAtional Flag Algorithm

--- Arrays/Medium/SortZeroesOnesandTwos.cpp
+++ Arrays/Medium/SortZeroesOnesandTwos.cpp
@@ -1,5 +1,5 @@
 // Given an array consisting of only 0s, 1s, and 2s. Write a program to in-place sort the array without using inbuilt sort functions.
-// this problem is also known as Dutch National Flag problem
+
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -7,27 +7,59 @@ using namespace std;
 // for brute approach use any in-place sorting algorithm available out there, like merge sort
 
 // better - Counting sort approach
+// void sort(vector<int> &arr){
+//     int n = arr.size();
+//     int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == 0) cnt0++;
+
+//         else if (arr[i] == 1) cnt1++;
+        
+//         else cnt2++;
+//     }
+
+//     for (int i = 0; i < cnt0; i++) arr[i] = 0;
+       
+//     for (int i = cnt0; i < cnt0 + cnt1; i++) arr[i] = 1;
+        
+//     for (int i = cnt0 + cnt1; i < n; i++)  arr[i] = 2;
+    
+// }
+
+
+// Optimal approach using the Dutch National flag algorithm
 void sort(vector<int> &arr){
     int n = arr.size();
-    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+    int low = 0; int mid = 0; int high = n - 1;
 
-    for (int i = 0; i < n; i++)
+    while (mid <= high)
     {
-        if (arr[i] == 0) cnt0++;
+        if (arr[mid] == 0)
+        {
+            int temp = arr[mid];
+            arr[mid] = arr[low];
+            arr[low] = temp;
 
-        else if (arr[i] == 1) cnt1++;
-        
-        else cnt2++;
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            int temp = arr[mid];
+            arr[mid] = arr[high];
+            arr[high] = temp;
+
+            high--;
+        }
     }
-
-    for (int i = 0; i < cnt0; i++) arr[i] = 0;
-       
-    for (int i = cnt0; i < cnt0 + cnt1; i++) arr[i] = 1;
-        
-    for (int i = cnt0 + cnt1; i < n; i++)  arr[i] = 2;
     
 }
-
 
 int main() {
     // Write your code here
