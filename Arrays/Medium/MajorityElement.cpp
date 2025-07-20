ed9596ab10242dca2dfa842f200c1854e3ef4836
gtr547 <gitarthapathak360@gmail.com>
2025-07-20 10:38:41 +0530

    Feat: Majority Element occurring more than n/2 times, solved(brute approach)

---
+++ Arrays/Medium/MajorityElement.cpp
@@ -0,0 +1,59 @@
+// Given an array of N integers, write a program to return an element that occurs more than N/2 times in the given array. 
+// You may consider that such an element always exists in the array.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// brute approach
+int majElem(vector<int> &arr){
+    int n =  arr.size();
+    
+    for (int i=0; i < n; i++)
+    {
+        int cnt = 0;
+        
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                cnt++;
+            }
+            
+        }
+        
+        if (cnt > (n/2))
+        {
+            return arr[i];
+        }
+        
+        
+    }
+    
+    return -1;
+    
+
+}
+
+
+int main() {
+    // Write your code here
+    vector<int> arr;
+    
+    int n = 0;
+
+    while (true)
+    {
+        cin>>n;
+
+        if(n != -1){
+            arr.push_back(n);
+        }
+        else break;
+    }
+
+    cout<<majElem(arr)<<endl;
+   
+        
+
+    return 0;
+}
