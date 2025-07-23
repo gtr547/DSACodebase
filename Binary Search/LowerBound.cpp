00c2fe2447539553f69a49cfe7b955c3c9b33ede
gtr547 <gitarthapathak360@gmail.com>
2025-07-23 23:58:03 +0530

    Feat: Finding lower bound of a number using binary serach

---
+++ Binary Search/LowerBound.cpp
@@ -0,0 +1,52 @@
+// Given a sorted array of N integers and an integer K, write a program to find the lower bound of K.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int>& arr, int k){
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] >= k)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+        
+    }
+    return ans;
+}
+
+int main() {
+    // Write your code here
+    vector<int> arr;
+    
+    int n = 0;
+
+    while (true)
+    {
+        cin>>n;
+
+        if(n != -1){
+            arr.push_back(n);
+        }
+        else break;
+    }
+    
+    int k = 0;
+    cin>>k;
+    
+    cout<<"The lower bound of "<<k<<" is "<<lowerBound(arr, k);
+   
+
+    return 0;
+}
