dd45e7584658cb9dc5ef2a546020a6b8ec9da407
gtr547 <gitarthapathak360@gmail.com>
2025-07-25 22:43:14 +0530

    Feat: Find First and Last Occurance of an element in a sorted array solved

---
+++ Binary Search/FirstAndLastOccurranceInAnArray.cpp
@@ -0,0 +1,108 @@
+// Given a sorted array of N integers, write a program to find the index of the last occurrence of the target key. If the target is not found then return -1.
+// Note: Consider 0 based indexing
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// simple approach using linear search
+// pair<int,int> ls(vector<int>& arr, int k){
+//     int n = arr.size();
+//     int first = -1, last = -1;
+    
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == k)
+//         {
+//             if (first == -1){
+//                 first = i;
+//             } 
+//             last = i;  
+//         }
+        
+//     }
+//     return make_pair(first, last);
+// }
+
+int lowerBound(vector<int>& arr, int k){
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] >= k)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+        
+    }
+    return ans;
+}
+
+int upperBound(vector<int>& arr, int k){
+    int n = arr.size();
+    int low = 0, high = n-1;
+    int ans = n;
+
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+
+        if(arr[mid] > k){
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+        
+    }
+    return ans;
+
+}
+
+pair<int,int> findFirstAndLastOccurrance(vector<int>& arr, int k){
+    int n = arr.size();
+    int lb = lowerBound(arr, k);
+    
+    if(lb == n || arr[lb] != k) return {-1, -1};
+    return{lb, upperBound(arr, k) - 1};
+    
+}
+
+
+
+int main() {
+    // Write your code here
+    vector<int> arr;
+    
+    int n = 0;
+
+    while (true)
+    {
+        cin>>n;
+
+        if(n != -1){
+            arr.push_back(n);
+        }
+        else break;
+    }
+    
+    int k = 0;
+    cin>>k;
+    
+    pair<int, int> ans = findFirstAndLastOccurrance(arr, k);
+
+    cout<<"["<<ans.first<<" "<<ans.second<<"]";
+    
+    
+    return 0;
+}
