6ffdc02eeac61ad55d6d16df2ccc2beb1d5e1e0c
gtr547 <gitarthapathak360@gmail.com>
2025-07-30 23:06:43 +0530

    Feat: Optimal Solution to Find the Square Root of an Integer using Binary Search

--- Binary Search/Binary Search On Answers/FindSquareRoot.cpp
+++ Binary Search/Binary Search On Answers/FindSquareRoot.cpp
@@ -9,27 +9,50 @@ using namespace std;
 
 
 // naive approach - O(N)
-int squareRoot(int n){
+// int squareRoot(int n){
 
+//     int ans = 1;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if (i * i <= n)
+//         {
+//             ans = i;
+//         }
+//         else
+//         {
+//             break;
+//         }
+    
+//     }
+//     return ans;
+
+// }
+
+// optimal approach using binary search
+int squareRoot(int n){
     int ans = 1;
 
-    for (int i = 1; i <= n; i++)
+    int low = 1, high = n;
+
+    while (low <= high)
     {
-        if (i * i <= n)
+        int mid = low + (high - low) / 2;
+
+        if (mid * mid <= n)
         {
-            ans = i;
+            ans = mid;
+            low = mid + 1;
         }
         else
         {
-            break;
+            high = mid - 1;
         }
-    
+         
     }
+    
     return ans;
-
 }
-
-
 
 int main() {
     // Write your code here
