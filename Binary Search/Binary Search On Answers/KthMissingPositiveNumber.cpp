60d7fd7243f6abc7f9d7da26d057111a121264c5
gtr547 <gitarthapathak360@gmail.com>
2025-08-04 22:32:20 +0530

    Feat: Optimal Solution to Kth Missing Positive Number

--- Binary Search/Binary Search On Answers/KthMissingPositiveNumber.cpp
+++ Binary Search/Binary Search On Answers/KthMissingPositiveNumber.cpp
@@ -5,15 +5,30 @@
 using namespace std;
 
 // naive approcach 
-int findK(vector<int>& arr, int k, int n){
-    for (int i = 0; i < n; i++)
-    {
-        if(arr[i]<=k) k++;
-        else break;
-    }
-    return k;
-}
+// int findK(vector<int>& arr, int k, int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//         if(arr[i]<=k) k++;
+//         else break;
+//     }
+//     return k;
+// }
 
+// optimal approach 
+int findK(vector<int>& arr, int k, int n){
+    int low = 0, high = n - 1;
+   
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        int missing = arr[mid] - (mid + 1);
+        
+        if(missing < k) low = mid + 1;
+        else high = mid - 1;
+    }
+
+    return (low + k);
+}
 
 int main() {
     // Write your code here
