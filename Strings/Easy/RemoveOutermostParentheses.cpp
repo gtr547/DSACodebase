8cfbd0c6b912e1d6a5ec3230795391395184b9f7
gtr547 <gitarthapathak360@gmail.com>
2025-08-08 00:06:20 +0530

    Feat: Optimal solution to Remove Outer Parentheses problem

--- Strings/Easy/RemoveOutermostParentheses.cpp
+++ Strings/Easy/RemoveOutermostParentheses.cpp
@@ -2,27 +2,54 @@
 using namespace std;
 
 // Naive approach T.C-O(N) S.C-O(N)
+// string removeOuterParentheses(string s) {
+//     int cnt = 0;
+//     string ans = "";
+
+//     for(int i = 0; i < s.length(); i++){
+//         if(s[i] == '('){
+//             if (cnt > 0)
+//             {
+//                 ans.push_back(s[i]);
+//             }
+//             cnt ++;
+//         }
+//         else if( s[i] == ')'){
+//             cnt --;
+//             if (cnt > 0)
+//             {
+//                 ans.push_back(s[i]);
+//             }
+//         }
+//     }
+//     return ans;
+// }
+
+// optimal approach T.C-O(N) S.C-O(1)
 string removeOuterParentheses(string s) {
     int cnt = 0;
-    string ans = "";
+    int i = 0, j = 0;
 
-    for(int i = 0; i < s.length(); i++){
-        if(s[i] == '('){
-            if (cnt > 0)
-            {
-                ans.push_back(s[i]);
+    while (i < s.length()) {
+        char c = s[i];
+
+        if (c == '(') {
+            if (cnt > 0) {
+                s[j] = c;
+                j++;
+            } 
+            cnt++;
+        } else {
+            cnt--;
+            if (cnt > 0) {
+                s[j] = c;
+                j++;
             }
-            cnt ++;
         }
-        else if( s[i] == ')'){
-            cnt --;
-            if (cnt > 0)
-            {
-                ans.push_back(s[i]);
-            }
-        }
+        i++;
     }
-    return ans;
+    s.resize(j);
+    return s;
 }
 
 
