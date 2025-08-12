9ef24a11ae93f958351815c37a17c76e4741d2d0
gtr547 <gitarthapathak360@gmail.com>
2025-08-12 23:12:29 +0530

    Feat: Converting an array to a linked list

---
+++ Linked Lists/1D Linked List/LinkedListImplementation.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+
+Node* convertToLL(vector<int> &arr){
+
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    
+    return head;
+
+}
+
+
+
+int main() {
+    // Write your code here
+    vector<int> arr = {92,5,8,7};
+
+    Node* head = convertToLL(arr);
+
+    cout<<head->data;
+
+    return 0;
+}
