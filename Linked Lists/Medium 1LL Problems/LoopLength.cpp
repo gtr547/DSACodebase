c669e4354353669b4b14a79b52aa8541fb9192bf
gtr547 <gitarthapathak360@gmail.com>
2025-08-19 23:36:33 +0530

    Feat: Optimal solution to Find the length of a cycle in a linked list using Floyd's Tortoise and Hare algorithm.

---
+++ Linked Lists/Medium 1LL Problems/LoopLength.cpp
@@ -0,0 +1,117 @@
+// Given the head of a linked list, return the length of the loop/cycle. If there is no cycle, 0.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+// Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. 
+// Note that pos is not passed as a parameter.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+
+};
+
+Node* toLL(vector<int> arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i = 1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+// creating cycles in a linked list
+Node* createCycle(Node* head, int k){
+    if(head == nullptr || k <= 0) return head;
+
+    Node* tail = head;
+    Node* cycleNode = nullptr;
+    int index = 1;
+
+    while(tail->next  != nullptr){
+        if(index == k){
+            cycleNode = tail;
+        }
+        tail = tail->next;
+        index++;
+    }
+
+    if(index == k){
+        cycleNode = tail;
+    }
+
+    if(cycleNode != nullptr){
+        tail->next = cycleNode;
+    }
+
+    return head;
+}
+
+// optimal solution to find the length of a cycle in a linked list using Flyods Tortoise and hare algorithm
+int startingNode(Node* head){
+    if(head == nullptr || head->next == nullptr) return 0;
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow == fast){
+            int cnt = 1;
+            slow = slow->next;
+
+            while (slow != fast)
+            {
+                cnt++;
+                slow = slow->next;
+            }
+
+            return cnt;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    // Write your code here
+    vector<int> arr;
+    
+    int n = 0;
+
+    while (true)
+    {
+        cin>>n;
+
+        if(n != -1){
+            arr.push_back(n);
+        }
+        else break;
+    }
+    
+    Node* head = toLL(arr);
+
+    int k;
+    cin>>k;
+
+    head = createCycle(head, k);
+
+    cout<<startingNode(head);
+    
+    return 0;
+}
