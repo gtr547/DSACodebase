d3f11bd5579e31dcecb98b56a04949d3a670c633
gtr547 <gitarthapathak360@gmail.com>
2025-08-24 00:17:22 +0530

    Update: Optimal Solution to Delete the Middle Node of a Linked list using Tortoise & Hare algorithm.

--- Linked Lists/Medium 1LL Problems/DeleteMiddleNode.cpp
+++ Linked Lists/Medium 1LL Problems/DeleteMiddleNode.cpp
@@ -4,15 +4,6 @@
 
 //     For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
 
-// Given head, the head of a linked list, determine if the linked list has a cycle in it.
-
-// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
-// Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
-
-// Return true if there is a cycle in the linked list. Otherwise, return false.
-
-// the naive approach to this problem consists of Hashing(map).
-
 #include <bits/stdc++.h>
 using namespace std;
 
